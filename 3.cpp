a3330f8632380a0c10b48295ac12db1e71226ee4
MOHD FARDEEN <fardeen777888666@gmail.com>
2026-07-17 18:11:00 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+    int n = s.size();
+    int low = 0;
+    int high = 0;
+    unordered_map<char, int> f;
+    int res = INT_MIN;
+    for (high = 0; high < n; high++)
+    {
+
+        f[s[high]]++;
+        int k = high - low + 1;
+        while (f.size() < k)
+        {
+            f[s[low]]--;
+            if (f[s[low]] == 0)
+            {
+                f.erase(s[low]);
+            }
+            low++;
+            k = high - low + 1;
+        }
+        int len = high - low + 1;
+        res = max(res, len);
+    }
+    if (res == INT_MIN)
+    {
+        return 0;
+    }
+    return res;
+}
