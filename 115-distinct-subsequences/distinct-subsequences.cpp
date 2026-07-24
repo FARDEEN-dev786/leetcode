80d1321857d18b1f8eff16ce248386772403d0d2
MOHD FARDEEN <fardeen777888666@gmail.com>
2026-07-24 14:30:30 +0530

    Time: 11 ms (87.38%) | Memory: 9.1 MB (81.16%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -5,38 +5,31 @@ public:
     int n = s.size();
     int m = t.size();
     // vector<vector<long long>> dp(n + 1);
-    vector<vector<unsigned long long>> dp(n + 1);
-    for (int i = 0; i <= n; i++)
-    {
-        // vector<long long> t(m + 1, 0);
-        vector<unsigned long long>t(m + 1, 0);
-        dp[i] = t;
-    }
+    vector<double> prev(m+1,0);
+    vector<double> next(m+1,0);
 
     
-    for (int i = 0; i <= n; i++)
-    {
-        dp[i][m] = 1;
-    }
-    for (int i = 0; i < m; i++)
-    {
-        dp[n][i] = 0;
-    }
+    prev[m]=1;
 
     for (int i = n - 1; i >= 0; i--)
     {
+        next[m]=1;
         for (int j = m - 1; j >= 0; j--)
         {
             if (s[i] == t[j])
             {
-                dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                //dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                next[j] = prev[j+1] + prev[j];
             }
             else
             {
-                dp[i][j] = dp[i + 1][j];
+                //dp[i][j] = dp[i + 1][j];
+                next[j] = prev[j];
             }
         }
+        prev = next;
     }
-    return dp[0][0];
+    //return dp[0][0];
+    return next[0];
 }
 };
