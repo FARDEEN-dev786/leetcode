aa9e2f3d88ceec5af3d0a5b9b58dea808380089e
MOHD FARDEEN <fardeen777888666@gmail.com>
2026-07-25 16:21:15 +0530

    Time: 18 ms (68.17%) | Memory: 63.6 MB (70.53%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -5,34 +5,31 @@ public:
     int n = prices.size();
     int k = 2;
     int i, j;
-    vector<vector<int>> dp(n + 1);
-    for (int i = 0; i <= n; i++)
-    {
-        vector<int> t(k + 1, -1);
-        dp[i] = t;
-    }
-    for (i = 0; i <= n; i++)
-        dp[i][0] = 0;
-    for (j = 0; j <= k; j++)
-        dp[n][j] = 0;
+    vector<int>next(3);
+    vector<int>curr(3);
+
+    for(int i = 0;i<=2;i++) next[i] = 0;
+    
     for (i = n - 1; i >= 0; i--)
     {
+        curr[0] = 0;
         for (j = 1; j <= k; j++)
         {
             if (j == 2)
             {
-                int c1 = dp[i + 1][j - 1] - prices[i];
-                int c2 = dp[i + 1][j];
-                dp[i][j] = max(c1, c2);
+                int c1 = next[j - 1] - prices[i];
+                int c2 = next[j];
+                curr[j] = max(c1, c2);
             }
             else
             {
-                int c1 = dp[i + 1][2] + prices[i] -fee;
-                int c2 = dp[i + 1][j];
-                dp[i][j] = max(c1, c2);
+                int c1 = next[2] + prices[i] -fee;
+                int c2 = next[j];
+                curr[j] = max(c1, c2);
             }
         }
+        next = curr;
     }
-    return dp[0][2];
+    return next[2];
 }
 };
