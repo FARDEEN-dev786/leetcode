c356bc4a3cb758b29379f1847e4ecedd87e0af0e
MOHD FARDEEN <fardeen777888666@gmail.com>
2026-07-30 23:48:34 +0530

    Time: 1 ms (83.07%) | Memory: 19.2 MB (67.95%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+{
+    vector<vector<int>> a(numCourses);
+    int i;
+    vector<int> indeg(numCourses, 0);
+    for (i = 0; i < prerequisites.size(); i++)
+    {
+        int src = prerequisites[i][0];
+        int dest = prerequisites[i][1];
+        a[src].push_back(dest);
+        indeg[dest]++;
+    }
+
+    queue<int> q;
+    for (i = 0; i < numCourses; i++)
+    {
+        if (indeg[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+
+    vector<int> res;
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        res.push_back(node);
+        for (int j = 0; j < a[node].size(); j++)
+        {
+            int neigh = a[node][j];
+            indeg[neigh]--;
+            if (indeg[neigh] == 0)
+            {
+                q.push(neigh);
+            }
+        }
+    }
+    if (res.size() == numCourses)
+        return true;
+    return false;
+}
+};
