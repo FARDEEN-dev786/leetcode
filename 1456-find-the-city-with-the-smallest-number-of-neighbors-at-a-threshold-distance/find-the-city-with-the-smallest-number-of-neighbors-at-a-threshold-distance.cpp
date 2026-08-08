06b02bbd6e20f22fd3826dd8a79ad51aaf0c8ac7
MOHD FARDEEN <fardeen777888666@gmail.com>
2026-08-08 15:49:04 +0530

    Time: 4 ms (99.44%) | Memory: 16.6 MB (71.02%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-    vector<vector<pair<int,int>>>a(n);
-    for(int i =0; i<edges.size();i++){
-        int s = edges[i][0];
-        int d = edges[i][1];
+    int findTheCity(int n, vector<vector<int>> &edges, int distanceThreshold)
+{
+    vector<vector<int>> dist(n, vector<int>(n, 1e9));
+    for (int i = 0; i < n; i++)
+    {
+        dist[i][i] = 0;
+    }
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
         int w = edges[i][2];
-        a[s].push_back({d,w});
-        a[d].push_back({s,w});
+        dist[u][v] = w;
+        dist[v][u] = w;
     }
-
-    vector<int>ans(n,0);
-    for(int i = 0; i<n;i++){
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        vector<int>dist(n,INT_MAX);
-        dist[i]=0;
-        pq.push({0,i});
-        while (!pq.empty())
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
         {
-            pair<int,int>p = pq.top();
-            pq.pop();
-            int d = p.first;
-            int node = p.second;
-            if(d>dist[node]) continue;
-            for(int j = 0; j<a[node].size(); j++){
-                int neigh = a[node][j].first;
-                int wt = a[node][j].second;
-                if(d+wt<dist[neigh]){
-                    dist[neigh]=wt+d;
-                    pq.push({d+wt,neigh});
-                }
+            for (int j = 0; j < n; j++)
+            {
+                dist[i][j] = min(
+                    dist[i][j],
+                    dist[i][k] + dist[k][j]);
             }
-
         }
-        int count= 0;
-        for(int j = 0; j<n;j++){
-            if(i==j) continue;
-            if(dist[j]<=distanceThreshold) count++;
-        }
-        ans[i]=count;  
     }
-    int answer=0;
-    for(int i =1; i<n;i++){
-        if(ans[i]<=ans[answer]) answer = i;
+    int answer = 0;
+    int smallest = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j && dist[i][j] <= distanceThreshold)
+            {
+                count++;
+            }
+        }
+
+        if (count <= smallest)
+        {
+            smallest = count;
+            answer = i;
+        }
     }
     return answer;
 }
